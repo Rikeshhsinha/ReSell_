a567faa5549fdc6ec8b8f9dd6f931482b55cf864
Rikeshhsinha <rikeshsinha245@gmail.com>
2026-05-19 00:47:11 +0530

    connect backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://resell-1-s487.onrender.com/api",
+  baseURL: "https://resell-4hzi.onrender.com/api",
 });
 
 export default API;
